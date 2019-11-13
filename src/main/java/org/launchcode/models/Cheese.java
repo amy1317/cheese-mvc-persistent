0f530f84d83439641dffead01c06dc83f7333415
amy1317 <fordamy953@gmail.com>
2019-11-13 10:14:29 -0500

    final code

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,10 +1,12 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import org.dom4j.Entity;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -17,14 +19,26 @@ public class Cheese {
     private int id;
 
     @NotNull
-    @Size(min=3, max=15)
+    @Size(min=3, max=15, message = "Name has to be between 3 to 15 characters long")
     private String name;
 
     @NotNull
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    //    @Null(message = "Please enter in a Category before adding a cheese")
+    @ManyToOne
+    private Category category;
+
+//    private CheeseType type;
 
     public Cheese(String name, String description) {
         this.name = name;
@@ -32,6 +46,9 @@ public class Cheese {
     }
 
     public Cheese() { }
+
+    @ManyToMany(mappedBy = "cheeses")
+    private List<Menu> menus;
 
     public int getId() {
         return id;
@@ -53,11 +70,11 @@ public class Cheese {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
-    }
-
-    public void setType(CheeseType type) {
-        this.type = type;
-    }
+//    public CheeseType getType() {
+//        return type;
+//    }
+//
+//    public void setType(CheeseType type) {
+//        this.type = type;
+//    }
 }
